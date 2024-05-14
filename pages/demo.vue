b70bd661be5232be8092a9c549d80a3a52130b80
JudyWu <judywu@sat.cool>
2024-05-14 10:30:41 +0800

    feat(package): 安裝 Sass 與 Normalize.css

---
+++ pages/demo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    Page: demo
+    <h3>SCSS</h3>
+    <div class="scss-text">Test 文字</div>
+    <hr>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss">
+.scss-text {
+  border: 1px solid #000;
+  @include font(#005CFF, 2, 500, 1.5);
+
+  @include tablet {
+    color: red;
+  }
+
+  @include mobile {
+    color: orange;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    Page: index
+    <NuxtLink to="/demo">Go to demo page</NuxtLink>
+  </div>
+</template>
